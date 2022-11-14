df283009cd6c2e0d77de182f4fca30d7791e5b08
tarsil <tiago.arasilva@gmail.com>
2022-11-14 17:32:52 +0000

    ➕ Add extra tests to trusted hosts middleware.

--- tests/middleware/test_trusted_host.py
+++ tests/middleware/test_trusted_host.py
@@ -5,6 +5,29 @@ from esmerald.responses import PlainTextResponse
 from esmerald import Gateway, get, Request
 
 
+def test_trusted_host_middleware_settings(test_client_factory):
+    @get()
+    def homepage(request: Request) -> PlainTextResponse:
+        return PlainTextResponse("OK", status_code=200)
+
+    app = Esmerald(
+        routes=[Gateway("/", handler=homepage)],
+        allowed_hosts=["testserver", "*.testserver"],
+    )
+
+    client = test_client_factory(app)
+    response = client.get("/")
+    assert response.status_code == 200
+
+    client = test_client_factory(app, base_url="http://subdomain.testserver")
+    response = client.get("/")
+    assert response.status_code == 200
+
+    client = test_client_factory(app, base_url="http://invalidhost")
+    response = client.get("/")
+    assert response.status_code == 400
+
+
 def test_trusted_host_middleware(test_client_factory):
     @get()
     def homepage(request: Request) -> PlainTextResponse:
@@ -13,7 +36,9 @@ def test_trusted_host_middleware(test_client_factory):
     app = Esmerald(
         routes=[Gateway("/", handler=homepage)],
         middleware=[
-            Middleware(TrustedHostMiddleware, allowed_hosts=["testserver", "*.testserver"])
+            Middleware(
+                TrustedHostMiddleware, allowed_hosts=["testserver", "*.testserver"]
+            )
         ],
     )
 
@@ -43,7 +68,9 @@ def test_www_redirect(test_client_factory):
 
     app = Esmerald(
         routes=[Gateway("/", handler=homepage)],
-        middleware=[Middleware(TrustedHostMiddleware, allowed_hosts=["www.example.com"])],
+        middleware=[
+            Middleware(TrustedHostMiddleware, allowed_hosts=["www.example.com"])
+        ],
     )
 
     client = test_client_factory(app, base_url="https://example.com")
