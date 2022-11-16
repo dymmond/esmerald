2caad9386959649ca40dd224c9ce12d107896c09
tarsil <tiago.arasilva@gmail.com>
2022-11-16 19:14:51 +0000

    ➕ Add more cors tests with the configurations.

--- tests/middleware/test_cors.py
+++ tests/middleware/test_cors.py
@@ -2,7 +2,7 @@ from esmerald.applications import Esmerald
 from starlette.middleware import Middleware
 from esmerald.middleware.cors import CORSMiddleware
 from esmerald.responses import PlainTextResponse
-from esmerald import Gateway, Request, get, route
+from esmerald import Gateway, Request, get, route, CORSConfig
 
 
 def test_cors_allow_all(test_client_factory):
@@ -115,6 +115,49 @@ def test_cors_allow_all_except_credentials(test_client_factory):
     assert "access-control-allow-origin" not in response.headers
 
 
+def test_cors_allow_specific_origin_with_config(test_client_factory):
+    @get()
+    def homepage(request: Request) -> PlainTextResponse:
+        return PlainTextResponse("Homepage", status_code=200)
+
+    cors_config = CORSConfig(
+        allow_origins=["https://example.org"],
+        allow_headers=["X-Example", "Content-Type"],
+    )
+    app = Esmerald(routes=[Gateway("/", handler=homepage)], cors_config=cors_config)
+
+    client = test_client_factory(app)
+
+    # Test pre-flight response
+    headers = {
+        "Origin": "https://example.org",
+        "Access-Control-Request-Method": "GET",
+        "Access-Control-Request-Headers": "X-Example, Content-Type",
+    }
+    response = client.options("/", headers=headers)
+    assert response.status_code == 200
+    assert response.text == "OK"
+    assert response.headers["access-control-allow-origin"] == "https://example.org"
+    assert response.headers["access-control-allow-headers"] == (
+        "Accept, Accept-Language, Content-Language, Content-Type, X-Example"
+    )
+    assert "access-control-allow-credentials" not in response.headers
+
+    # Test standard response
+    headers = {"Origin": "https://example.org"}
+    response = client.get("/", headers=headers)
+    assert response.status_code == 200
+    assert response.text == "Homepage"
+    assert response.headers["access-control-allow-origin"] == "https://example.org"
+    assert "access-control-allow-credentials" not in response.headers
+
+    # Test non-CORS response
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.text == "Homepage"
+    assert "access-control-allow-origin" not in response.headers
+
+
 def test_cors_allow_specific_origin(test_client_factory):
     @get()
     def homepage(request: Request) -> PlainTextResponse:
@@ -203,6 +246,41 @@ def test_cors_disallowed_preflight(test_client_factory):
     assert response.text == "Disallowed CORS headers"
 
 
+def test_cors_disallowed_preflight_with_config(test_client_factory):
+    @get()
+    def homepage(request: Request) -> None:
+        pass  # pragma: no cover
+
+    cors_config = CORSConfig(
+        allow_origins=["https://example.org"],
+        allow_headers=["X-Example"],
+    )
+    app = Esmerald(routes=[Gateway("/", handler=homepage)], cors_config=cors_config)
+
+    client = test_client_factory(app)
+
+    # Test pre-flight response
+    headers = {
+        "Origin": "https://another.org",
+        "Access-Control-Request-Method": "POST",
+        "Access-Control-Request-Headers": "X-Nope",
+    }
+    response = client.options("/", headers=headers)
+    assert response.status_code == 400
+    assert response.text == "Disallowed CORS origin, headers"
+    assert "access-control-allow-origin" not in response.headers
+
+    # Bug specific test, https://github.com/encode/starlette/pull/1199
+    # Test preflight response text with multiple disallowed headers
+    headers = {
+        "Origin": "https://example.org",
+        "Access-Control-Request-Method": "GET",
+        "Access-Control-Request-Headers": "X-Nope-1, X-Nope-2",
+    }
+    response = client.options("/", headers=headers)
+    assert response.text == "Disallowed CORS headers"
+
+
 def test_preflight_allows_request_origin_if_origins_wildcard_and_credentials_allowed(
     test_client_factory,
 ):
@@ -239,6 +317,37 @@ def test_preflight_allows_request_origin_if_origins_wildcard_and_credentials_all
     assert response.headers["vary"] == "Origin"
 
 
+def test_preflight_allows_request_origin_if_origins_wildcard_and_credentials_allowed_config(
+    test_client_factory,
+):
+    @get()
+    def homepage(request: Request) -> None:
+        return  # pragma: no cover
+
+    cors_config = CORSConfig(
+        allow_origins=["*"],
+        allow_methods=["POST"],
+        allow_credentials=True,
+    )
+    app = Esmerald(routes=[Gateway("/", handler=homepage)], cors_config=cors_config)
+
+    client = test_client_factory(app)
+
+    # Test pre-flight response
+    headers = {
+        "Origin": "https://example.org",
+        "Access-Control-Request-Method": "POST",
+    }
+    response = client.options(
+        "/",
+        headers=headers,
+    )
+    assert response.status_code == 200
+    assert response.headers["access-control-allow-origin"] == "https://example.org"
+    assert response.headers["access-control-allow-credentials"] == "true"
+    assert response.headers["vary"] == "Origin"
+
+
 def test_cors_preflight_allow_all_methods(test_client_factory):
     @get()
     def homepage(request: Request) -> None:
@@ -248,6 +357,27 @@ def test_cors_preflight_allow_all_methods(test_client_factory):
         routes=[Gateway("/", handler=homepage)],
         middleware=[Middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"])],
     )
+
+    client = test_client_factory(app)
+
+    headers = {
+        "Origin": "https://example.org",
+        "Access-Control-Request-Method": "POST",
+    }
+
+    for method in ("DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"):
+        response = client.options("/", headers=headers)
+        assert response.status_code == 200
+        assert method in response.headers["access-control-allow-methods"]
+
+
+def test_cors_preflight_allow_all_methods_config(test_client_factory):
+    @get()
+    def homepage(request: Request) -> None:
+        pass  # pragma: no cover
+
+    config = CORSConfig(allow_origins=["*"])
+    app = Esmerald(routes=[Gateway("/", handler=homepage)], cors_config=config)
 
     client = test_client_factory(app)
 
@@ -414,6 +544,24 @@ def test_cors_credentialed_requests_return_specific_origin(test_client_factory):
     assert "access-control-allow-credentials" not in response.headers
 
 
+def test_cors_credentialed_requests_return_specific_origin_config(test_client_factory):
+    @get()
+    def homepage(request: Request) -> PlainTextResponse:
+        return PlainTextResponse("Homepage", status_code=200)
+
+    cors = CORSConfig(allow_origins=["*"])
+    app = Esmerald(routes=[Gateway("/", handler=homepage)], cors_config=cors)
+    client = test_client_factory(app)
+
+    # Test credentialed request
+    headers = {"Origin": "https://example.org", "Cookie": "star_cookie=sugar"}
+    response = client.get("/", headers=headers)
+    assert response.status_code == 200
+    assert response.text == "Homepage"
+    assert response.headers["access-control-allow-origin"] == "https://example.org"
+    assert "access-control-allow-credentials" not in response.headers
+
+
 def test_cors_vary_header_defaults_to_origin(test_client_factory):
     @get()
     def homepage(request: Request) -> PlainTextResponse:
@@ -433,6 +581,23 @@ def test_cors_vary_header_defaults_to_origin(test_client_factory):
     assert response.headers["vary"] == "Origin"
 
 
+def test_cors_vary_header_defaults_to_origin_config(test_client_factory):
+    @get()
+    def homepage(request: Request) -> PlainTextResponse:
+        return PlainTextResponse("Homepage", status_code=200)
+
+    cors = CORSConfig(allow_origins=["https://example.org"])
+    app = Esmerald(routes=[Gateway("/", handler=homepage)], cors_config=cors)
+
+    headers = {"Origin": "https://example.org"}
+
+    client = test_client_factory(app)
+
+    response = client.get("/", headers=headers)
+    assert response.status_code == 200
+    assert response.headers["vary"] == "Origin"
+
+
 def test_cors_vary_header_is_not_set_for_non_credentialed_request(test_client_factory):
     @get()
     def homepage(request: Request) -> PlainTextResponse:
@@ -442,6 +607,22 @@ def test_cors_vary_header_is_not_set_for_non_credentialed_request(test_client_fa
         routes=[Gateway("/", handler=homepage)],
         middleware=[Middleware(CORSMiddleware, allow_origins=["*"])],
     )
+    client = test_client_factory(app)
+
+    response = client.get("/", headers={"Origin": "https://someplace.org"})
+    assert response.status_code == 200
+    assert response.headers["vary"] == "Accept-Encoding"
+
+
+def test_cors_vary_header_is_not_set_for_non_credentialed_request_config(
+    test_client_factory,
+):
+    @get()
+    def homepage(request: Request) -> PlainTextResponse:
+        return PlainTextResponse("Homepage", status_code=200, headers={"Vary": "Accept-Encoding"})
+
+    cors = CORSConfig(allow_origins=["*"])
+    app = Esmerald(routes=[Gateway("/", handler=homepage)], cors_config=cors)
     client = test_client_factory(app)
 
     response = client.get("/", headers={"Origin": "https://someplace.org"})
@@ -485,6 +666,27 @@ def test_cors_vary_header_is_properly_set_when_allow_origins_is_not_wildcard(
     assert response.headers["vary"] == "Accept-Encoding, Origin"
 
 
+def test_cors_vary_header_is_properly_set_when_allow_origins_is_not_wildcard_config(
+    test_client_factory,
+):
+    @get()
+    def homepage(request: Request) -> PlainTextResponse:
+        return PlainTextResponse("Homepage", status_code=200, headers={"Vary": "Accept-Encoding"})
+
+    cors = CORSConfig(allow_origins=["https://example.org"])
+    app = Esmerald(
+        routes=[
+            Gateway("/", handler=homepage),
+        ],
+        cors_config=cors,
+    )
+    client = test_client_factory(app)
+
+    response = client.get("/", headers={"Origin": "https://example.org"})
+    assert response.status_code == 200
+    assert response.headers["vary"] == "Accept-Encoding, Origin"
+
+
 def test_cors_allowed_origin_does_not_leak_between_credentialed_requests(
     test_client_factory,
 ):
@@ -504,6 +706,39 @@ def test_cors_allowed_origin_does_not_leak_between_credentialed_requests(
                 allow_methods=["*"],
             )
         ],
+    )
+
+    client = test_client_factory(app)
+    response = client.get("/", headers={"Origin": "https://someplace.org"})
+    assert response.headers["access-control-allow-origin"] == "*"
+    assert "access-control-allow-credentials" not in response.headers
+
+    response = client.get("/", headers={"Cookie": "foo=bar", "Origin": "https://someplace.org"})
+    assert response.headers["access-control-allow-origin"] == "https://someplace.org"
+    assert "access-control-allow-credentials" not in response.headers
+
+    response = client.get("/", headers={"Origin": "https://someplace.org"})
+    assert response.headers["access-control-allow-origin"] == "*"
+    assert "access-control-allow-credentials" not in response.headers
+
+
+def test_cors_allowed_origin_does_not_leak_between_credentialed_requests_config(
+    test_client_factory,
+):
+    @get()
+    def homepage(request: Request) -> PlainTextResponse:
+        return PlainTextResponse("Homepage", status_code=200)
+
+    cors = CORSConfig(
+        allow_origins=["*"],
+        allow_headers=["*"],
+        allow_methods=["*"],
+    )
+    app = Esmerald(
+        routes=[
+            Gateway("/", handler=homepage),
+        ],
+        cors_config=cors,
     )
 
     client = test_client_factory(app)
