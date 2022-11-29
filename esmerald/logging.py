74d5ea64c3708246965bf51c6abaf183e549f352
tarsil <tiago.arasilva@gmail.com>
2022-11-29 17:28:49 +0000

    🪛 Update pyproject requirements

--- esmerald/logging.py
+++ esmerald/logging.py
@@ -9,13 +9,11 @@ logger = logging.getLogger("esmerald")
 
 class InterceptHandler(logging.Handler):
     def emit(self, record: logging.LogRecord) -> None:
-        # Get corresponding Loguru level if it exists
         try:
             level: str = loguru_logger.level(record.levelname).name
         except ValueError:
             level: str = str(record.levelno)
 
-        # Find caller from where originated the logged message
         frame, depth = logging.currentframe(), 2
         while frame.f_code.co_filename == logging.__file__:  # noqa: WPS609
             frame = cast(FrameType, frame.f_back)
