f2a258744bae5174cb1054939c644e5e41ec413a
tarsil <tiago.arasilva@gmail.com>
2023-01-18 13:03:14 +0000

    ➕ Add file and Sform params

--- esmerald/params.py
+++ esmerald/params.py
@@ -1,8 +1,9 @@
 from typing import Any, Dict, Optional, Union
 
+from pydantic.fields import FieldInfo, Undefined
+
 from esmerald.enums import EncodingType, ParamType
 from esmerald.utils.constants import IS_DEPENDENCY, SKIP_VALIDATION
-from pydantic.fields import FieldInfo, Undefined
 
 
 class Param(FieldInfo):
@@ -345,6 +346,90 @@ class Body(FieldInfo):
             min_length=min_length,
             max_length=max_length,
             regex=regex,
+            **extra,
+        )
+
+
+class Form(Body):
+    def __init__(
+        self,
+        default: Any,
+        *,
+        media_type: Union[str, EncodingType] = EncodingType.URL_ENCODED,
+        content_encoding: Optional[str] = None,
+        alias: Optional[str] = None,
+        title: Optional[str] = None,
+        description: Optional[str] = None,
+        gt: Optional[float] = None,
+        ge: Optional[float] = None,
+        lt: Optional[float] = None,
+        le: Optional[float] = None,
+        min_length: Optional[int] = None,
+        max_length: Optional[int] = None,
+        regex: Optional[str] = None,
+        example: Any = Undefined,
+        examples: Optional[Dict[str, Any]] = None,
+        **extra: Any,
+    ):
+        super().__init__(
+            default=default,
+            embed=True,
+            media_type=media_type,
+            content_encoding=content_encoding,
+            alias=alias,
+            title=title,
+            description=description,
+            gt=gt,
+            ge=ge,
+            lt=lt,
+            le=le,
+            min_length=min_length,
+            max_length=max_length,
+            regex=regex,
+            example=example,
+            examples=examples,
+            **extra,
+        )
+
+
+class File(Form):
+    def __init__(
+        self,
+        default: Any,
+        *,
+        media_type: Union[str, EncodingType] = EncodingType.MULTI_PART,
+        content_encoding: Optional[str] = None,
+        alias: Optional[str] = None,
+        title: Optional[str] = None,
+        description: Optional[str] = None,
+        gt: Optional[float] = None,
+        ge: Optional[float] = None,
+        lt: Optional[float] = None,
+        le: Optional[float] = None,
+        min_length: Optional[int] = None,
+        max_length: Optional[int] = None,
+        regex: Optional[str] = None,
+        example: Any = Undefined,
+        examples: Optional[Dict[str, Any]] = None,
+        **extra: Any,
+    ):
+        super().__init__(
+            default=default,
+            embed=True,
+            media_type=media_type,
+            content_encoding=content_encoding,
+            alias=alias,
+            title=title,
+            description=description,
+            gt=gt,
+            ge=ge,
+            lt=lt,
+            le=le,
+            min_length=min_length,
+            max_length=max_length,
+            regex=regex,
+            example=example,
+            examples=examples,
             **extra,
         )
 
