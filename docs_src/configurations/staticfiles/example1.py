9d9c3ca61fcfaceea7a6aa7595fab52a25f47e6e
Tiago Silva <tiago.arasilva@gmail.com>
2023-01-18 19:16:44 +0000

    🪛 Static example one

--- docs_src/configurations/staticfiles/example1.py
+++ docs_src/configurations/staticfiles/example1.py
@@ -1,7 +1,9 @@
 from esmerald import Esmerald, StaticFilesConfig
 
+from pathlib import Path
+
 static_files_config = StaticFilesConfig(
-    path="/static", packages=["mypackage"], directory="/static"
+    path="/static", packages=["mypackage"], directory=Path("static")
 )
 
 app = Esmerald(static_files_config=static_files_config)
