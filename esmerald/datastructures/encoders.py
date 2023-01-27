e780701de5ea7cddd20a8c84e68fa64a4a7a8c42
Tiago Silva <tiago.arasilva@gmail.com>
2023-01-27 22:19:15 +0000

    🔧Update the encoders datastructure

--- esmerald/datastructures/encoders.py
+++ esmerald/datastructures/encoders.py
@@ -1,22 +1,64 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, Dict, Optional, Type, Union
 
 from esmerald.datastructures.base import ResponseContainer  # noqa
 from esmerald.datastructures.json import JSON
 
 if TYPE_CHECKING:
-    pass
+    from esmerald.applications import Esmerald
+    from esmerald.enums import MediaType
 
 try:
-    from esmerald.responses.encoders import ORJSONResponse, UJSONResponse
+    from esmerald.responses.encoders import ORJSONResponse
 except ImportError:
-    raise ImportError(
-        "You must install the encoders to use URJSON and OrJSON. You can do it with `pip install esmerald[encoders]`"
-    )
+    ORJSONResponse = None
+
+try:
+    from esmerald.responses.encoders import UJSONResponse
+except ImportError:
+    UJSONResponse = None
 
 
-class OrJSON(JSON, ResponseContainer[ORJSONResponse]):
-    ...
+class OrJSON(ResponseContainer[ORJSONResponse]):
+    content: Optional[Dict[str, Any]] = None
+    status_code: Optional[int] = None
+
+    def to_response(
+        self,
+        headers: Dict[str, Any],
+        media_type: Union["MediaType", str],
+        status_code: int,
+        app: Type["Esmerald"],
+    ) -> ORJSONResponse:
+        assert ORJSONResponse is not None, "You must install the encoders or orjson to use ORJSONResponse"
+        status = self.status_code or status_code
+        
+        return ORJSONResponse(
+            content=self.content,
+            headers=headers,
+            status_code=status,
+            media_type=media_type,
+            background=self.background,
+        )
 
 
-class UJSON(JSON, ResponseContainer[UJSONResponse]):
-    ...
+class UJSON(ResponseContainer[UJSONResponse]):
+    content: Optional[Dict[str, Any]] = None
+    status_code: Optional[int] = None
+
+    def to_response(
+        self,
+        headers: Dict[str, Any],
+        media_type: Union["MediaType", str],
+        status_code: int,
+        app: Type["Esmerald"],
+    ) -> ORJSONResponse:
+        assert UJSONResponse is not None, "You must install the encoders or ujson to use UJSONResponse"
+        status = self.status_code or status_code
+        
+        return UJSONResponse(
+            content=self.content,
+            headers=headers,
+            status_code=status,
+            media_type=media_type,
+            background=self.background,
+        )
