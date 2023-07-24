23cdb4479a437c2f119d7e1a349316ec93a088c4
tarsil <tiago.arasilva@gmail.com>
2023-07-24 09:24:55 +0100

    Fix zoneinfo using pytz

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from zoneinfo import ZoneInfo
 
+import pytz
 from freezegun import freeze_time
 
 from esmerald.security.utils import convert_time
@@ -19,7 +19,7 @@ def test_convert_time():
 
 @freeze_time("2023-01-01 18:05:20")
 def test_convert_time_tz_info():
-    now = datetime.now(tz=ZoneInfo("Europe/Rome"))
+    now = datetime.now(tz=pytz.timezone("Europe/Rome"))
 
     time = convert_time(now)
 
