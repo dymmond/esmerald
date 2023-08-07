b880479bf4ce7686898000f45b282682501b29f6
tarsil <tiago.arasilva@gmail.com>
2023-08-07 22:35:29 +0100

    📃 Add docs for Edgy

--- docs_src/databases/edgy/set_password.py
+++ docs_src/databases/edgy/set_password.py
@@ -1,12 +1,18 @@
 from pydantic import EmailStr
 
-from esmerald.contrib.auth.tortoise.base_user import User as BaseUser
+from esmerald.conf import settings
+from esmerald.contrib.auth.edgy.base_user import User as BaseUser
+
+database, models = settings.registry
 
 
 class User(BaseUser):
     """
     Inherits from the BaseUser all the fields and adds extra unique ones.
     """
+
+    class Meta:
+        registry = models
 
     def __str__(self):
         return f"{self.email} - {self.last_login}"
@@ -17,6 +23,6 @@ async def set_password(email: EmailStr, password: str) -> None:
     """
     Set the password of a user is correct.
     """
-    user: User = await User.get(email=email)
+    user: User = await User.query.get(email=email)
 
     await user.set_password(password)
