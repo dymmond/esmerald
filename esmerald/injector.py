4cb4b19aaa8d87cf464faecd1a5a2a8bd5fb3379
Shako Rzayev <shahriyar.rzayev@nordsec.com>
2023-09-21 00:00:14 +0200

    feat: naive Factory implementation for getting rid of lambdas from Inject

--- esmerald/injector.py
+++ esmerald/injector.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Any, Dict, Optional, Type
+from typing import TYPE_CHECKING, Any, Dict, Optional, Tuple, Type
 
 from esmerald.parsers import ArbitraryHashableBaseModel
 from esmerald.transformers.datastructures import Signature
@@ -7,6 +7,28 @@ from esmerald.utils.helpers import is_async_callable
 
 if TYPE_CHECKING:  # pragma: no cover
     from esmerald.typing import AnyCallable
+
+
+class Factory:
+    def __init__(self, provides: "AnyCallable", *args: Any) -> None:
+        self.provides = provides
+        self.__args: Tuple[Any, ...] = ()
+        self.set_args(*args)
+
+    def set_args(self, *args: Any) -> None:
+        self.__args = args
+
+    @property
+    def cls(self) -> "AnyCallable":
+        """Return provided type."""
+        return self.provides
+
+    async def __call__(self) -> Any:
+        if is_async_callable(self.provides):
+            value = await self.provides(*self.__args)
+        else:
+            value = self.provides(*self.__args)
+        return value
 
 
 class Inject(ArbitraryHashableBaseModel):
