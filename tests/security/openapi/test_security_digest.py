f01237fc6e9363905e2dfc2086f3e9060cdcf7fe
tarsil <tiago.arasilva@gmail.com>
2023-10-05 20:10:03 +0100

    skip ci: Add tests to security openapi

---
+++ tests/security/openapi/test_security_digest.py
@@ -0,0 +1,156 @@
+from typing import Dict, List, Union
+
+import pytest
+from pydantic import BaseModel
+
+from esmerald import Gateway, JSONResponse, get
+from esmerald.openapi.security.http import Digest
+from esmerald.testclient import create_client
+
+
+class Error(BaseModel):
+    status: int
+    detail: str
+
+
+class CustomResponse(BaseModel):
+    status: str
+    title: str
+    errors: List[Error]
+
+
+class JsonResponse(JSONResponse):
+    media_type: str = "application/vnd.api+json"
+
+
+class Item(BaseModel):
+    sku: Union[int, str]
+
+
+@get("/item/{id}")
+async def read_item(id: str) -> None:
+    """ """
+
+
+@pytest.mark.parametrize("auth", [Digest, Digest()])
+def test_security_digest(auth):
+    @get(
+        response_class=JsonResponse,
+        security=[auth],
+    )
+    def read_people() -> Dict[str, str]:
+        """ """
+
+    with create_client(
+        routes=[Gateway(handler=read_item), Gateway(handler=read_people)],
+        enable_openapi=True,
+        include_in_schema=True,
+    ) as client:
+        response = client.get("/openapi.json")
+        assert response.json() == {
+            "openapi": "3.1.0",
+            "info": {
+                "title": "Esmerald",
+                "summary": "Esmerald application",
+                "description": "Highly scalable, performant, easy to learn and for every application.",
+                "contact": {"name": "admin", "email": "admin@myapp.com"},
+                "version": client.app.version,
+            },
+            "servers": [{"url": "/"}],
+            "paths": {
+                "/item/{id}": {
+                    "get": {
+                        "summary": "Read Item",
+                        "operationId": "read_item_item__id__get",
+                        "parameters": [
+                            {
+                                "name": "id",
+                                "in": "path",
+                                "required": True,
+                                "deprecated": False,
+                                "allowEmptyValue": False,
+                                "allowReserved": False,
+                                "schema": {"type": "string", "title": "Id"},
+                            }
+                        ],
+                        "responses": {
+                            "200": {"description": "Successful response"},
+                            "422": {
+                                "description": "Validation Error",
+                                "content": {
+                                    "application/json": {
+                                        "schema": {
+                                            "$ref": "#/components/schemas/HTTPValidationError"
+                                        }
+                                    }
+                                },
+                            },
+                        },
+                        "deprecated": False,
+                    }
+                },
+                "/": {
+                    "get": {
+                        "summary": "Read People",
+                        "operationId": "read_people__get",
+                        "deprecated": False,
+                        "security": [
+                            {
+                                "Digest": {
+                                    "type": "http",
+                                    "name": "Authorization",
+                                    "in": "header",
+                                    "scheme": "digest",
+                                    "scheme_name": "Digest",
+                                }
+                            }
+                        ],
+                        "responses": {
+                            "200": {
+                                "description": "Successful response",
+                                "content": {
+                                    "application/vnd.api+json": {"schema": {"type": "string"}}
+                                },
+                            }
+                        },
+                    }
+                },
+            },
+            "components": {
+                "schemas": {
+                    "HTTPValidationError": {
+                        "properties": {
+                            "detail": {
+                                "items": {"$ref": "#/components/schemas/ValidationError"},
+                                "type": "array",
+                                "title": "Detail",
+                            }
+                        },
+                        "type": "object",
+                        "title": "HTTPValidationError",
+                    },
+                    "ValidationError": {
+                        "properties": {
+                            "loc": {
+                                "items": {"anyOf": [{"type": "string"}, {"type": "integer"}]},
+                                "type": "array",
+                                "title": "Location",
+                            },
+                            "msg": {"type": "string", "title": "Message"},
+                            "type": {"type": "string", "title": "Error Type"},
+                        },
+                        "type": "object",
+                        "required": ["loc", "msg", "type"],
+                        "title": "ValidationError",
+                    },
+                },
+                "securitySchemes": {
+                    "Digest": {
+                        "type": "http",
+                        "name": "Authorization",
+                        "in": "header",
+                        "scheme": "digest",
+                    }
+                },
+            },
+        }
