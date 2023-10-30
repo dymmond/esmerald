5ce9716d340f3b7f47d64bec3ef647db72400026
tarsil <tiago.arasilva@gmail.com>
2023-10-30 18:30:59 +0000

    Replace random module for secrets

--- esmerald/utils/crypto.py
+++ esmerald/utils/crypto.py
@@ -1,4 +1,5 @@
-import random
+import secrets
+from typing import Optional
 
 
 def get_random_string(
@@ -10,12 +11,12 @@ def get_random_string(
     The default length of 12 with the a-z, A-Z, 0-9 character set returns
     a 71-bit value. log_2((26+26+10)^12) =~ 71 bits
     """
-    return "".join(random.choice(allowed_chars) for _ in range(length))
+    return "".join(secrets.choice(allowed_chars) for _ in range(length))
 
 
-def get_random_secret_key() -> str:
+def get_random_secret_key(length: Optional[int] = 32) -> str:
     """
     Return a 50 character random string usable as a SECRET_KEY setting value.
     """
     chars = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)"
-    return get_random_string(32, chars)
+    return get_random_string(length, chars)
