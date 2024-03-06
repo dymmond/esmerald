4d8332d2807726e3adcc3ccb5fe923f7d5628a8b
tarsil <tiago.arasilva@gmail.com>
2024-03-06 10:15:41 +0000

    Refine requirements

--- esmerald/middleware/wsgi.py
+++ esmerald/middleware/wsgi.py
@@ -1,3 +1,12 @@
-from a2wsgi.wsgi import WSGIMiddleware  # noqa
+from __future__ import annotations
+
+try:
+    from a2wsgi.wsgi import WSGIMiddleware as WSGIMiddleware  # noqa
+except ModuleNotFoundError:
+    raise RuntimeError(
+        "You need to install the package `a2wsgi` to be able to use this middleware. "
+        "Simply run `pip install a2wsgi`."
+    ) from None
+
 
 __all__ = ["WSGIMiddleware"]
