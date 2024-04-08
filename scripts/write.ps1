0da4c4f54fd9170a05553123e6e5c0839e7a1977
Omelchenko Michael <socanime@gmail.com>
2024-04-08 14:41:19 +0300

    fix write.ps1 Ignore venv .venv

--- scripts/write.ps1
+++ scripts/write.ps1
@@ -1,12 +1,15 @@
 # Loop through all .py files in the current directory and its subdirectories
-foreach ($file in Get-ChildItem -Path . -Recurse -Filter '*.py' -File) {
-    # Skip __init__.py and __main__.py files
-    if ($file.Name -ne '__init__.py' -and $file.Name -ne '__main__.py') {
-        # Display the file path
-        Write-Host $file.FullName
-        # Add "from __future__ import annotations" to the beginning of the file
-        (Get-Content $file.FullName) | Foreach-Object {
-            $_ -replace '^', 'from __future__ import annotations`n'
-        } | Set-Content $file.FullName
+Get-ChildItem -Path . -Recurse -Filter '*.py' -File | ForEach-Object {
+    # Check if the file path contains 'venv' or '.venv', and skip those files
+    if ($_.FullName -notmatch '\\venv\\|\\.venv\\') {
+        # Skip __init__.py and __main__.py files
+        if ($_.Name -ne '__init__.py' -and $_.Name -ne '__main__.py') {
+            # Display the file path
+            Write-Host $_.FullName
+            # Add "from __future__ import annotations" to the beginning of the file
+            (Get-Content $_.FullName) | Foreach-Object {
+                $_ -replace '^', 'from __future__ import annotations`n'
+            } | Set-Content $_.FullName
+        }
     }
 }
