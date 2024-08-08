1d8e90f454d46f4320236dbc36e981f534a7ab57
tarsil <tiago.arasilva@gmail.com>
2024-08-08 12:46:15 +0200

    Unpin the databasez version

--- tests/databases/saffier/test_middleware_payload.py
+++ tests/databases/saffier/test_middleware_payload.py
@@ -58,7 +58,7 @@ async def create_test_database():
 
 @pytest.fixture(autouse=True)
 async def rollback_transactions():
-    with database.force_rollback():
+    with database.force_rollback(False):
         async with database:
             yield
 
