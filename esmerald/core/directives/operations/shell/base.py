ca30ee7e285d633a51a8823dc16d34dbde14cc7a
tarsil <tiago.arasilva@gmail.com>
2024-11-14 10:55:57 +0100

    Fix shell loading default

--- esmerald/core/directives/operations/shell/base.py
+++ esmerald/core/directives/operations/shell/base.py
@@ -1,8 +1,12 @@
 import select
 import sys
+from collections.abc import Sequence
+from typing import Any, Callable
 
 import click
-from lilya.cli.directives.operations.shell.base import handle_lifespan_events, run_shell
+import nest_asyncio
+from lilya._internal._events import AyncLifespanContextManager
+from lilya.cli.directives.operations.shell.enums import ShellOption
 from lilya.compat import run_sync
 
 from esmerald.core.directives.env import DirectiveEnv
@@ -37,3 +41,31 @@ def shell(env: DirectiveEnv, kernel: bool) -> None:
     )
     run_sync(run_shell(env.app, lifespan, kernel))  # type: ignore
     return None
+
+
+async def run_shell(app: Any, lifespan: Any, kernel: str) -> None:
+    """Executes the database shell connection"""
+
+    async with lifespan(app):
+        if kernel == ShellOption.IPYTHON:
+            from esmerald.core.directives.operations.shell.ipython import get_ipython
+
+            ipython_shell = get_ipython(app=app)
+            nest_asyncio.apply()
+            ipython_shell()
+        else:
+            from esmerald.core.directives.operations.shell.ptpython import get_ptpython
+
+            ptpython = get_ptpython(app=app)
+            nest_asyncio.apply()
+            ptpython()
+
+
+def handle_lifespan_events(
+    on_startup: Sequence[Callable] | None = None,
+    on_shutdown: Sequence[Callable] | None = None,
+    lifespan: Any | None = None,
+) -> Any:
+    if lifespan:
+        return lifespan
+    return AyncLifespanContextManager(on_startup=on_startup, on_shutdown=on_shutdown)
