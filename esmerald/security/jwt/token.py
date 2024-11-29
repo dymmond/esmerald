ee94d4e58ab135597808cdfca80ff781782b9d10
Tiago Silva <tiago.arasilva@gmail.com>
2024-11-29 15:54:38 +0000

    Update function signature

--- esmerald/security/jwt/token.py
+++ esmerald/security/jwt/token.py
@@ -47,37 +47,39 @@ class Token(BaseModel):
             raise ValueError(f"{subject} is not a valid string.") from e
 
     def encode(
-        self, key: str, algorithm: str, **payload_extra: Any
+        self,
+        key: str,
+        algorithm: str,
+        claims_extra: Union[Dict[str, Any], None] = None,
+        **kwargs: Any,
     ) -> Union[str, Any]:  # pragma: no cover
         """
         Encodes the token into a proper str formatted and allows passing kwargs.
         """
-        payload: Dict[str, Any] = {**self.model_dump(exclude_none=True), **payload_extra}
+        if claims_extra is None:
+            claims_extra = {}
+
+        payload: Dict = {**self.model_dump(exclude_none=True), **claims_extra}
         try:
             return jwt.encode(
                 payload=payload,
                 key=key,
                 algorithm=algorithm,
+                **kwargs,
             )
         except PyJWTError as e:
             raise ImproperlyConfigured("Error encoding the token.") from e
 
     @classmethod
     def decode(
-        cls,
-        token: str,
-        key: Union[str, bytes, jwt.PyJWK],
-        algorithms: List[str],
+        cls, token: str, key: Union[str, bytes, jwt.PyJWK], algorithms: List[str], **kwargs: Any
     ) -> "Token":  # pragma: no cover
         """
         Decodes the given token.
         """
         try:
             data = jwt.decode(
-                jwt=token,
-                key=key,
-                algorithms=algorithms,
-                options={"verify_aud": False},
+                jwt=token, key=key, algorithms=algorithms, options={"verify_aud": False}, **kwargs
             )
         except PyJWTError as e:
             raise e
