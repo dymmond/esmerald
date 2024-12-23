bd9018e33acc3ee1e858c517eb7652a80bba7c34
alex <devkral@web.de>
2024-12-23 23:06:24 +0100

    Changes:
    - rename test methods
    - add test for route with GET and POST

--- tests/test_edgecases_responses.py
+++ tests/test_edgecases_responses.py
@@ -1,14 +1,20 @@
 import os
 
-from esmerald import Esmerald, Redirect, Request, Template
+from pydantic import BaseModel
+
+from esmerald import Esmerald, Form, Redirect, Request, Template
 from esmerald.config.template import TemplateConfig
 from esmerald.responses.base import RedirectResponse
 from esmerald.routing.gateways import Gateway
-from esmerald.routing.handlers import get
+from esmerald.routing.handlers import get, route
 from esmerald.testclient import EsmeraldTestClient
 
 
-def test_issue1(template_dir, test_client_factory):
+class Model(BaseModel):
+    id: str
+
+
+def test_return_response_container(template_dir):
     path = os.path.join(template_dir, "start.html")
     with open(path, "w") as file:
         file.write("<html>Hello, <a href='{{ url_for('homepage') }}'>world</a></html>")
@@ -23,13 +29,14 @@ def test_issue1(template_dir, test_client_factory):
         template_config=TemplateConfig(
             directory=template_dir,
         ),
+        middleware=[],
     )
     client = EsmeraldTestClient(app)
     response = client.get("/")
     assert response.status_code == 301
 
 
-def test_issue2(template_dir, test_client_factory):
+def test_return_response(template_dir):
     path = os.path.join(template_dir, "start.html")
     with open(path, "w") as file:
         file.write("<html>Hello, <a href='{{ url_for('homepage') }}'>world</a></html>")
@@ -48,3 +55,17 @@ def test_issue2(template_dir, test_client_factory):
     client = EsmeraldTestClient(app)
     response = client.get("/")
     assert response.status_code == 301
+
+
+def test_get_and_post():
+    @route(methods=["GET", "POST"])
+    async def start(request: Request, data: Model | None = Form()) -> bytes:
+        return b"hello world"
+
+    app = Esmerald(
+        debug=True,
+        routes=[Gateway("/", handler=start)],
+    )
+    client = EsmeraldTestClient(app)
+    response = client.get("/")
+    assert response.status_code == 200
