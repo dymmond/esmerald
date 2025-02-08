6ba9b7b069b404daf12b64b3a1339c5ee584851c
tarsil <tiago.arasilva@gmail.com>
2025-02-08 22:47:20 +0100

    Update mypy

--- esmerald/permissions/utils.py
+++ esmerald/permissions/utils.py
@@ -1,7 +1,9 @@
-from typing import TYPE_CHECKING, Callable, Optional
+from typing import TYPE_CHECKING, Any, Callable, Optional, Union, cast
+
+from lilya.permissions.base import DefinePermission
 
 from esmerald.exceptions import PermissionDenied
-from esmerald.utils.helpers import is_async_callable
+from esmerald.utils.helpers import is_async_callable, is_class_and_subclass
 
 if TYPE_CHECKING:  # pragma: no cover
     from esmerald.permissions import BasePermission
@@ -40,3 +42,37 @@ def permission_denied(request: "Request", message: Optional[str] = None) -> None
     If request is not permitted, determine what kind of exception to raise.
     """
     raise PermissionDenied(detail=message, status_code=403)
+
+
+def is_esmerald_permission(permission: Union["BasePermission", Any]) -> bool:
+    """
+    Checks if the given permission is an instance or subclass of BasePermission.
+
+    Args:
+        permission (Union["BasePermission", Any]): The permission to check.
+    Returns:
+        bool: True if the permission is an instance or subclass of BasePermission, False otherwise.
+    """
+
+    from esmerald.permissions import BasePermission
+
+    return is_class_and_subclass(permission, BasePermission)
+
+
+def wrap_permission(
+    permission: Union["BasePermission", Any],
+) -> "BasePermission":
+    """
+    Wraps the given permission into a BasePermission instance if it is not already one.
+    Or else it will assume its a Lilya permission and wraps it.
+
+    Args:
+        permission (Union["BasePermission", Any]): The permission to be wrapped.
+    Returns:
+        BasePermission: The wrapped permission instance.
+    """
+
+    if is_esmerald_permission(permission):
+        return permission
+
+    return cast("BasePermission", DefinePermission(cast(Any, permission)))
