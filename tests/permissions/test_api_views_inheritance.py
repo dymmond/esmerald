3c89262e1abc390f8e81af9742819bbddc9d7cf9
tarsil <tiago.arasilva@gmail.com>
2025-02-16 00:22:44 +0100

    Add extra test to level

--- tests/permissions/test_api_views_inheritance.py
+++ tests/permissions/test_api_views_inheritance.py
@@ -1,5 +1,5 @@
 from esmerald import APIView, get
-from esmerald.permissions import AllowAny, DenyAll
+from esmerald.permissions import AllowAny, DenyAll, IsAuthenticated
 from esmerald.testclient import create_client
 
 
@@ -14,9 +14,18 @@ class MyAPIView(APIView):
 class MySimpleAPIView(MyAPIView):
     permissions = [AllowAny]
 
-    @get("/new-home")
+    @get(
+        "/new-home",
+    )
     async def get(self) -> str:
         return "home simple"
+
+
+class AnotherAPI(MyAPIView):
+    permissions = [AllowAny]
+
+    @get("/another", permissions=[IsAuthenticated])
+    async def get(self) -> str: ...
 
 
 def test_cannot_access_apiview(test_client_factory):
@@ -33,3 +42,15 @@ def test_cannot_access_simple_apiview(test_client_factory):
 
         assert response.status_code == 403
         assert MySimpleAPIView.permissions == [DenyAll, AllowAny]
+
+
+def test_inheritance_total(test_client_factory):
+    with create_client(routes=[AnotherAPI], enable_openapi=False) as client:
+        response = client.get("/another")
+
+        assert response.status_code == 403
+        assert AnotherAPI.permissions == [DenyAll, AllowAny]
+
+        permissions = client.app.routes[0].handler._application_permissions
+
+        assert len(permissions) == 3
