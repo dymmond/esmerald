868ee27d65dcd4c5d092c2f7f0527ce2376cd0c3
alex <devkral@web.de>
2025-09-13 20:47:32 +0200

    fix crypto imports

--- esmerald/utils/crypto.py
+++ esmerald/utils/crypto.py
@@ -1,3 +1,3 @@
-from lilya.crypto import get_random_secret_key, get_random_string
+from lilya._internal._crypto import get_random_secret_key, get_random_string
 
 __all__ = ["get_random_string", "get_random_secret_key"]
